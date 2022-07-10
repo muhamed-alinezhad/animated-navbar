2596292cd81444706e7eae172acafe5366722231
muhamed <muhamed.alinezhad.1376@gmail.com>
2022-07-10 10:32:18 +0430

    initial project

---
+++ animated-navbar.js
@@ -0,0 +1,7 @@
+fullpage = document.querySelector(".fullpage-menu");
+icon = document.querySelector(".icon")
+
+var animation = function(){
+    fullpage.classList.toggle('active');
+    icon.classList.toggle('active');
+}
